068b6c8ee894059e7fb296e1f1063609193291b7
qiuyunan <qiuyunan@126.com>
2016-07-08 19:51:16 +0800

    update 002

--- src/testgithub/com/TestDeom.java
+++ src/testgithub/com/TestDeom.java
@@ -1,8 +1,9 @@
 package testgithub.com;
 
 public class TestDeom {
- public static void main(String[] args) {
-	System.out.println("this is test giyhub");
-	System.out.println("this is test giyhub02");
-}
+	public static void main(String[] args) {
+		System.out.println("this is test giyhub");
+		System.out.println("this is test giyhub02");
+		System.out.println("this is test giyhub03");
+	}
 }
